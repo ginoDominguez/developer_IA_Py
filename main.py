a46e922f5e759d643105cb87ea1abdb6b76089a6
gino dominguez <gino_2do@hotmail.com>
2024-10-10 12:42:00 +0200

    Doing a template

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
-
-### Moer exercises:
-
-
-mass_percentage = "1/6"
-print("On the Moon, you would weigh about %s of your weight on Earth." % mass_percentage)
+### exercises about strings:
 
 
-print("""Both sides of the %s get the same amount of sunlight, but only one side is seen from %s because the %s rotates around its own axis when it orbits %s.""" % ("Moon", "Earth", "Moon", "Earth"))
+name= 'Ganymede'
+planet= 'Mars'
+gravity= 1.43
+
+template= f"""Gravity Facts about {name}
+{'-' * 20}
+Planet Name: {planet}
+Gravity on {name}: {gravity} m/s2 """
 
 
-mass_percentage = 1/6
-print("""You are lighter on the {moon}, because on the {moon} you would weigh about {mass} of your weight on Earth.""".format(moon="Moon", mass=mass_percentage))
-
-
-print(f"On the Moon, you would weigh about {mass_percentage} of your weight on Earth.")
-
-
-### Redondeando:
-print(f"On the Moon, you would weigh about {round(mass_percentage,2)} of your weight on Earth.")
-
-
+print(template.format(name=name, planet=planet, gravity=gravity))
