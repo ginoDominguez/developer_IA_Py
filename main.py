6cc055441fefe86caf79d16bcbff3f28e53dd2b6
Gino Dominguez <gino_2do@hotmail.com>
2024-10-14 14:01:29 +0200

    test raiseExceptions

--- main.py
+++ main.py
@@ -1,31 +1,17 @@
 ### Raise exceptions
 
-def alert_navigation_system(err):
-    print(err)
+true_values = ['yes', 'y']
+false_values = ['no', 'n']
 
-
-def water_left(astronauts, water_left, days_left):
-    for argument in [astronauts, water_left, days_left]:
-        try:
-            # If argument is an int, the following operation will work
-            argument / 10
-        except TypeError:
-            # TypeError will be raised only if it isn't the right type 
-            # Raise the same exception but with a better error message
-            raise TypeError(f"All arguments must be of type int, but received: '{argument}'")
-    daily_usage = astronauts * 11
-    total_usage = daily_usage * days_left
-    total_water_left = water_left - total_usage
-    if total_water_left < 0:
-        raise RuntimeError(f"There is not enough water for {astronauts} astronauts after {days_left} days!")
-    return f"Total water left after {days_left} days is: {total_water_left} liters"
-
-
-try:
-    water_left(5,100,2)
-except RuntimeError as err:
-    alert_navigation_system(err)
-
-
-###print(water_left(3,50,10))
+def str_to_bool(value):
+    value = value.lower()
+    if value in true_values:
+        return True
+    elif value in false_values:
+        return False
+    else:
+        raise ValueError('Invalid entry')
+    
+    
+str_to_bool("yes")
 
