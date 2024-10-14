21e86def93dce5154b6f6e33f7eefe79c1f008ff
Gino Dominguez <gino_2do@hotmail.com>
2024-10-14 12:51:14 +0200

    Errors management

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
-##### Handle exceptions
+### Raise exceptions
 
-def main():
-    try:
-        open("/path/to/mars.jpg")
-    except FileNotFoundError:
-        print("Coldn't find the config.txt")
-    except IsADirectoryError:
-        print("Found config.txt but it is a directory, couldn't read it")
-    except Exception as e:
-        print(type(e))
-    finally:
-        print("Todo correcto")
-            
+def alert_navigation_system(err):
+    print(err)
 
-main()
+
+def water_left(astronauts, water_left, days_left):
+    for argument in [astronauts, water_left, days_left]:
+        try:
+            # If argument is an int, the following operation will work
+            argument / 10
+        except TypeError:
+            # TypeError will be raised only if it isn't the right type 
+            # Raise the same exception but with a better error message
+            raise TypeError(f"All arguments must be of type int, but received: '{argument}'")
+    daily_usage = astronauts * 11
+    total_usage = daily_usage * days_left
+    total_water_left = water_left - total_usage
+    if total_water_left < 0:
+        raise RuntimeError(f"There is not enough water for {astronauts} astronauts after {days_left} days!")
+    return f"Total water left after {days_left} days is: {total_water_left} liters"
+
+
+try:
+    water_left(5,100,2)
+except RuntimeError as err:
+    alert_navigation_system(err)
+
+
+###print(water_left(3,50,10))
 
